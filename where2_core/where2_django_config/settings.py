59c74a2f80e519fb9e2ced40174ff66ad2078406
Oden Baker <88295456+1LooperTrooper@users.noreply.github.com>
2022-03-31 08:43:50 +1000

    Update settings.py

--- where2_core/where2_django_config/settings.py
+++ where2_core/where2_django_config/settings.py
@@ -20,14 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qq&ioa)7wgly5#jeo5z11^aw0h9i)#^mljyru9^9918#d6x$2i'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
